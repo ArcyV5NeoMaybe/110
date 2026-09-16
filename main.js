95fe023d55535f861cfae23ee0c8c3da042d7f85
Thailander Kind Hero <eyehdhsndbsbd@gmail.com>
2026-09-16 20:13:59 +0700

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded",()=>{
-    const panel=document.getElementById("panel");
-    const flagButton=document.getElementById("flagButton");
-    if(flagButton&&panel){
-        flagButton.addEventListener("click",()=>panel.classList.toggle("open"));
-    }
-});
